b831540dbe81672630116ed40e956d38a89e0b72
saim ali <saim.ali@inaf.it>
2025-07-17 18:44:16 +0200

    made some modification in implementation and also remove the if statement in startfilter

--- src/VisIVOFilters/Filters/vspointdistributeop.cpp
+++ src/VisIVOFilters/Filters/vspointdistributeop.cpp
@@ -704,7 +704,7 @@ bool VSPointDistributeOp::execute()
     int rank, size;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
-    printf("op.execute() %d of %d processes \n", rank, size);
+    printf("op.execute %d of %d processes \n", rank, size);
     
     //Processes data in chunks until all elements (totEle) are handled
     while(totEle!=0)
